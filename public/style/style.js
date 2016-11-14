57f3c8813631ed58a2200ea442ec0ee4736b2472
xrey007 <reinaldo.leal001@gmail.com>
2016-11-14 10:30:37 -0500

    validating css/html

--- public/style/style.js
+++ public/style/style.js
@@ -1,4 +1,4 @@
-
+//
 var mq = window.matchMedia( "(min-width: 500px)" );
 
 if(mq.matches) {
@@ -22,32 +22,40 @@ if(mq.matches) {
 }
 
 
-
-
-  // if (matchMedia) {
-  //   var mq = window.matchMedia("(min-width: 500px)");
-  //   mq.addListener(WidthChange);
-  //   WidthChange(mq);
-  // }
-  //
-  // function WidthChange(mq) {
-  //   if(mq.matches) {
-  //     function openNav() {
-  //       document.getElementById("mySidenav").style.width = "400px";
-  //       document.getElementById("main1").style.marginLeft = "400px";
-  //       document.getElementById("main2").style.marginLeft = "400px";
-  //     }
-  //     function closeNav() {
-  //       document.getElementById("mySidenav").style.width = "0";
-  //       document.getElementById("main1").style.marginLeft = "0";
-  //       document.getElementById("main2").style.marginLeft = "0";
-  //     }
-  //   } else {
-  //     function openNav() {
-  //       document.getElementById("mySidenav").style.width = "100%";
-  //     }
-  //     function closeNav() {
-  //       document.getElementById("mySidenav").style.width = "0";
-  //     }
-  //   }
-  // }
+// console.log("Hello");
+//
+// $(document).ready(function(){
+//   console.log(matchMedia);
+//
+//   if (matchMedia) {
+//     var mq = window.matchMedia("(min-width: 500px)");
+//     console.log(WidthChange);
+//     mq.addListener(WidthChange, function(){console.log("Hello1 width");});
+//     WidthChange(mq);
+//   }
+//
+//   console.log("Hello1");
+//   function WidthChange(mq) {
+//     console.log("Hello11");
+//     console.log(mq);
+//     if(mq.matches) {
+//       function openNav() {
+//         document.getElementById("mySidenav").style.width = "400px";
+//         document.getElementById("main1").style.marginLeft = "400px";
+//         document.getElementById("main2").style.marginLeft = "400px";
+//       }
+//       function closeNav() {
+//         document.getElementById("mySidenav").style.width = "0";
+//         document.getElementById("main1").style.marginLeft = "0";
+//         document.getElementById("main2").style.marginLeft = "0";
+//       }
+//     } else {
+//       function openNav() {
+//         document.getElementById("mySidenav").style.width = "100%";
+//       }
+//       function closeNav() {
+//         document.getElementById("mySidenav").style.width = "0";
+//       }
+//     }
+//   }
+// });
