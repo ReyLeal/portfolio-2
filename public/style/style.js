78325b25d512b3cbb19c96b3dcc5e9cc96bf958d
xrey007 <reinaldo.leal001@gmail.com>
2016-11-14 11:45:20 -0500

    removing texts from js

--- public/style/style.js
+++ public/style/style.js
@@ -1,23 +1,15 @@
 
-
-console.log("Hello");
-
 $(document).ready(function(){
-  console.log(matchMedia);
 
   if (matchMedia) {
     var mq = window.matchMedia("(min-width: 500px)");
-    console.log(WidthChange);
     mq.addListener(WidthChange, function(){console.log("Hello1 width");});
     WidthChange(mq);
   }
 
   console.log("Hello1");
   function WidthChange(mq) {
-    console.log("Hello11");
-    console.log(mq);
     if(mq.matches) {
-      console.log("Hello100");
       $('#onclick').click(function(){
           document.getElementById("mySidenav").style.width = "400px";
           document.getElementById("main1").style.marginLeft = "400px";
